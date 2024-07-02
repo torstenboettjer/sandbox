b867f960892c04db60c27be194848193a619166f
Torsten Boettjer <torsten.boettjer@gmail.com>
2024-07-02 18:02:36 +0200

    initial load

--- home.nix
+++ home.nix
@@ -5,12 +5,9 @@
       hello
     ];
 
-    # This needs to actually be set to your username
     username = "torsten";
     homeDirectory = "/home/torsten";
 
-    # You do not need to change this if you're reading this in the future.
-    # Don't ever change this after the first build.  Don't ask questions.
     stateVersion = "23.11";
   };
 }
