461fddfcee81c602080c46fd3f316e305c687f27
Torsten Boettjer <57480261+hcops@users.noreply.github.com>
2024-07-06 12:23:50 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-# Call with 'sh <(curl -L https://raw.githubusercontent.com/hcops/repository/main/launch.sh <username>)'
+# Call with 'sh <(curl -L https://raw.githubusercontent.com/hcops/repository/main/launch.sh)'
 
-# Ask for a username
-
+# Ask for a username and store the input in a variable
+echo "Enter a username:"
+read USERNAME
 
 # Parameters
 CONTAINER_NAME="nix"
 DISTRO="debian/bookworm"
-USERNAME=$1
-CONTAINER_SIZE="85G"  # Example size
+CONTAINER_SIZE="85G"  # Suggested size
 
 # Enable the Linux (Beta) environment
 vmc start termina
@@ -25,6 +25,9 @@ lxc exec ${CONTAINER_NAME} -- adduser --disabled-password --gecos "" ${USERNAME}
 # Optionally install additional packages
 lxc exec ${CONTAINER_NAME} -- apt-get update
 lxc exec ${CONTAINER_NAME} -- apt-get install -y build-essential
+
+# Create a configuration file
+echo ${USERNAME} > username.txt
 
 # Exit
 exit
