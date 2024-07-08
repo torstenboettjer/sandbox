a4bc687ce8f1c662ae5222674403638a422dd412
Torsten Boettjer <torsten.boettjer@gmail.com>
2024-07-08 15:34:28 +0200

    Retriev gh api output for file configuration

--- hminstall.sh
+++ hminstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Still draft -don't use
 
 # Exit on error
 set -e
@@ -7,26 +6,12 @@ set -e
 # create log file
 touch ./setup.log
 
-# https://nixos.org/
-# if command -v nix &> /dev/null; then
-#     echo "Installing nix single user mode..."
-#     curl -LO https://nixos.org/nix/install
-#     chmod +x ./install
-#     ./install --daemon --yes
-#     . $HOME/.nix-profile/etc/profile.d/nix.sh
-#     echo "--> done"
-#     NIXVERSION=$(nix --version)
-#     echo "$NIXVERSION is installed." >> ./setup.log
-# else
-#     NIXVERSION=$(nix --version)
-#     echo "$NIXVERSION is installed." >> ./setup.log
-# fi
-
 # clone the default home-manager configuration 
-gh repo clone hcops/workspace
+nix-shell -p gh --run "gh api user > $HOME/ghacc.json"
+nix-shell -p gh --run "gh repo clone hcops/workspace"
 
 # activating experimental features
-echo -e "experimental-features = nix-command flakes\ntrusted-users = root torsten" | sudo tee -a /etc/nix/nix.conf
+echo -e "experimental-features = nix-command flakes\ntrusted-users = root ${USER}" | sudo tee -a /etc/nix/nix.conf
 
 # add the home-manager package channel
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
@@ -43,5 +28,5 @@ echo -e '. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"' >> $HOME/.prof
 # test the installation
 source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh && home-manager --version
 
-# remove nix install script
-# rm ./install
+# activate home manager
+home-manager switch --flake .#"${USER}"
