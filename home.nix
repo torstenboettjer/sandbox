e6c6389618949ac783eb5c43b05d3a75b7c15fe7
hcops <torsten.boettjer@gmail.com>
2024-09-17 07:23:45 +0200

    FIX: Resolve captive browser import issue

--- home.nix
+++ home.nix
@@ -142,10 +142,6 @@ in
     bash.enable = true;
   };
 
-  imports = [
-    ./captivebrowser.nix
-  ];
-
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
